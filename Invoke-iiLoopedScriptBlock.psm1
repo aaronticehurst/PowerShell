67650bfd94ec45ebe89c7020404d2aba3f451b50
Aaron <aaron_ticehurst@hotmail.com>
2017-03-12 11:47:00 +0800

    Updated comments

--- Invoke-iiLoopedScriptBlock.psm1
+++ Invoke-iiLoopedScriptBlock.psm1
@@ -1,48 +1,47 @@
 ﻿Function Invoke-iiLoopedScriptBlock {
 
-     <#
+    <#
      .SYNOPSIS
           Loops a scriptbock
      .DESCRIPTION
             Simple looping function to loop a command block so you don't need to keep running it yourself.
      .PARAMETER  Scriptblock
           The block of code you want to loop
-    .PARAMETER  Loops
+     .PARAMETER  Loops
           Finite number of loops to run the scriptblock
-    .PARAMETER  SecondsSleep
+     .PARAMETER  SecondsSleep
           Number of seconds between each loop
-         .EXAMPLE
-                  PS C:\> Invoke-iiLoopedCommand { Resolve-DnsName sma.staff.iinet.net.au} -Loops 100 -SecondsSleep 10
-          Check diskspace of isp-osb-dfs1
+      .EXAMPLE
+          PS C:\> Invoke-iiLoopedCommand { Resolve-DnsName google.com} -Loops 100 -SecondsSleep 10
+          Loops command until expiration
      .NOTES
           Written by Aaron Ticehurst 24/5/2016.
 #>
 
 
-param([CmdletBinding()]
+    param([CmdletBinding()]
 
-  [Parameter(Mandatory=$true,
-                    ValueFromPipelineByPropertyName=$true,
-                    ValueFromPipelineByProperty=$true,
-                    Position=0)]                    
-	                [scriptblock]$Scriptblock,
+        [Parameter(Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ValueFromPipelineByProperty=$true,
+            Position=0)]                    
+        [scriptblock]$Scriptblock,
         [Parameter(Mandatory=$False,
-                    ValueFromPipelineByPropertyName=$true,
-                    ValueFromPipelineByProperty=$true,
-                    Position=1)]
-                    [INT]$Loops=10,
+            ValueFromPipelineByPropertyName=$true,
+            ValueFromPipelineByProperty=$true,
+            Position=1)]
+        [INT]$Loops=10,
         [Parameter(Mandatory=$False,
-                    ValueFromPipelineByPropertyName=$true,
-                    ValueFromPipelineByProperty=$true,
-                    Position=2)]
+            ValueFromPipelineByPropertyName=$true,
+            ValueFromPipelineByProperty=$true,
+            Position=2)]
         [INT]$SecondsSleep = 60
- )
+    )
 
-         for ($i = 1; $i -le $Loops; $i++)
-        { 
-                    $Scriptblock.Invoke()
-                    Start-Sleep -Seconds $SecondsSleep
-        }
+    for ($i = 1; $i -le $Loops; $i++) { 
+        $Scriptblock.Invoke()
+        Start-Sleep -Seconds $SecondsSleep
+    }
 
 }
 
