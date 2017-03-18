ea5806eea082b6d22ef8bc4c60b321576fdcce73
Aaron <aaron_ticehurst@hotmail.com>
2017-03-18 11:02:11 +0800

    Re-organise Folders

---
+++ Modules/Support/Get-DiskSpace.psm1
@@ -0,0 +1,70 @@
+﻿Function Get-DiskSpace {
+
+    <#
+     .SYNOPSIS
+          Gets disk space of a computer.
+     .DESCRIPTION
+            Gets disk space of a computer.
+     .PARAMETER  ComputerName
+          The Computer that you are targeting.
+	 .EXAMPLE
+	 	  PS C:\> Get-DiskSpace.ps1 -ComputerName TestComputer | ft -a
+          Check diskspace of TestComputer
+     .NOTES
+          Written by Aaron Ticehurst 6/12/2012.
+		  #>
+
+    param(
+
+        [CmdletBinding()]
+
+        [parameter(ValueFromPipelineByPropertyName=$true,
+            ValueFromPipeline=$true)]
+        [alias("name")]
+        [string[]]$ComputerName = $env:computername,
+        [parameter(ValueFromPipelineByPropertyName=$true,
+            ValueFromPipeline=$true)]
+        [string]$Drive,
+        [Parameter()]
+        [pscredential]$Credential
+				)	
+    Begin {	
+    }
+    Process {
+
+        Foreach ($Computer in $ComputerName ) {
+            $Drive = $Drive + '%'
+            #Get's Computer Hard Drive stats 
+            if ( $Drive) {
+                $PSBoundParameters.Remove('Drive') | Out-Null
+            }   
+            $Device = get-wmiobject Win32_logicalDisk -Filter "drivetype = '3' and deviceID like `'$Drive`'" @PSBoundParameters 
+
+            Foreach ($Device1 in $Device) {
+
+                $hash = @{ 
+                    ComputerName = $Device1.__Server
+                    Drive = $Device1.DeviceID
+                    VolumeName = $device1.VolumeName
+                    Size = [Math]::Round($Device1.size/1GB,0)
+                    FreeSpace = [Math]::Round($Device1.Freespace/1GB,0)
+                    PercentageFree = [Math]::Round($Device1.Freespace/$Device1.size*100,2)
+			
+			
+                }
+                $Object = New-Object PSObject -Property $hash
+                Write-Output $Object | Select ComputerName, drive, VolumeName, Size, FreeSpace, PercentageFree
+				
+				
+	
+            }
+        }
+	
+    }
+		
+    End {	
+    }		
+
+}
+New-Alias -name GDS -value Get-DiskSpace
+Export-ModuleMember Get-DiskSpace -Alias GDS
---
+++ Scripts/Set-iiLogon.ps1
@@ -0,0 +1,98 @@
+﻿<#
+     .SYNOPSIS
+          Records staff logging onto a computer.
+     .DESCRIPTION
+          Records staff logging onto a computer.
+  	 .NOTES
+          Written by Aaron Ticehurst 17/10/2016. 
+		  		  
+#>
+
+$Username =$env:username
+$Domain = $env:USERDOMAIN
+$ComputerName = $env:computername
+
+Try {
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+$objSearcher.SearchRoot = $objDomain
+
+
+$objSearcher.Filter = ("(&(objectClass=User)(SamAccountName=$Username))")
+$UserResults = $objSearcher.Findone()
+
+$objSearcher.Filter = ("(&(objectClass=computer)(name=$ComputerName))")
+$ComputerResults = $objSearcher.Findone()
+
+}
+
+Catch {
+
+$Fail = @"
+Set-iiLogon
+Failed to read AD
+
+$error
+
+$(Get-WmiObject Win32_NetworkAdapterConfiguration  | where-object {$_.ipEnabled -match "True"} |Select IPAddress  | out-string)
+"@
+
+
+
+
+        $EventLog = @{ 
+			            LogName = 'Application'
+				        Source = 'Application Error'
+				        Message = "$($fail)"
+				        EntryType = 'Information'
+				        EventID = 1000
+				        ComputerName = "$($env:ComputerName)"
+				        }
+				        Write-EventLog @EventLog
+}
+
+If ($UserResults -and $ComputerResults) {
+Try {
+                $hash = @{
+                UserID = & { $UserObjectGUID = $UserResults.Properties.objectguid ; (new-object guid(,$UserObjectGUID[0])).Guid -replace '-' }
+                Logon = "$Domain\$Username"
+                ServerGUID = & { $ComputerObjectGUID = $ComputerResults.Properties.objectguid ; (new-object guid(,$ComputerObjectGUID[0])).Guid -replace '-' }
+                ServerName = $ComputerName
+                LogonTime = (get-date).ToString("yyyy-MM-dd HH:mm:ss")
+                                }
+                $Object = New-Object -TypeName PSObject -Property $hash
+
+
+                [string]$ConnectionString = "Server=XXXXXXX;Database=XXXXXXX;Integrated Security=True;Connect Timeout=30"
+                $connection = New-Object -TypeName System.Data.SqlClient.SqlConnection
+			    $connection.ConnectionString = $connectionString
+                $connection.Open()
+			    $command = $connection.CreateCommand()
+                $command.CommandText ="INSERT tblUserLogons VALUES ('$($object.UserID)', '$($object.Logon)', '$($object.ServerGUID)', '$($Object.ServerName)', '$($object.LogonTime)' )"
+                $command.ExecuteNonQuery() | Out-Null
+                $connection.Close()
+
+}
+Catch{
+$Fail = @"
+Set-iiLogon
+Failed to write to database
+
+$($hash |out-string)
+
+$error
+
+$(Get-WmiObject Win32_NetworkAdapterConfiguration  | where-object {$_.ipEnabled -match "True"} |Select IPAddress | out-string)
+"@
+
+        $EventLog = @{ 
+				        LogName = 'Application'
+				        Source = 'Application Error'
+				        Message = "$($fail)"
+				        EntryType = 'Error'
+				        EventID = 1000
+				        ComputerName = "$($env:ComputerName)"
+				        }
+				        Write-EventLog @EventLog
+}
+}
