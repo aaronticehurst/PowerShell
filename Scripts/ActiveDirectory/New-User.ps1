9d26c8dd0ef6f2f6668a570a857240ff5a55b292
Aaron <aaron_ticehurst@hotmail.com>
2017-04-08 12:08:55 +0800

    Initial Commit

---
+++ Scripts/ActiveDirectory/New-User.ps1
@@ -0,0 +1,124 @@
+<#
+     .SYNOPSIS
+          Creates new users from HR SQL into AD.
+     .DESCRIPTION
+          Creates new users from HR SQL into AD.
+     .PARAMETER  Server
+          Remote DC to connect to.
+     .PARAMETER	Database
+	 	  SQL server database containing data. 
+	 .PARAMETER	Credential
+	 	  Remote AD credentials.
+     .PARAMETER	ShowProgress
+	 	  Show progress if running from command line.        
+	  .EXAMPLE
+	      PS C:\> .\New-User.ps1 -Credential $creds
+		  Creates new users in Active Directory
+     .NOTES
+          Written by Aaron Ticehurst 7/4/2017.
+#>
+[cmdletbinding(SupportsShouldProcess = $True)]
+param(
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 0)]
+    [String]$Server = 'ticehurst.local',
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 1)]
+    [String]$StagingOU = "OU=ii,OU=People,OU=Staff,DC=ticehurst,DC=local",
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 2)]
+    [String]$SQLServer = 'sql2',
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 3)]
+    [String]$Database = 'ITS',
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 4)]
+    [pscredential]$Credential,
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 5)]
+    [bool]$ShowProgress = $False
+
+
+)
+if (-not (Test-Path c:\Logs\HR_Sync)) {New-Item -Path c:\Logs\HR_Sync -ItemType Directory}
+$LogFolder = "c:\Logs\HR_Sync"
+$LogFile = "HR_NewUser.$(Get-date -format ddMMyyyyhhmmss).log"
+$LogPath = Join-Path -path $LogFolder -ChildPath $LogFile
+$NewLog = New-Item -Path $LogPath -ItemType File 
+Write-Verbose "LogFile: $LogPath on $env:ComputerName"
+
+$Domain = ($env:USERDNSDOMAIN).ToLower()
+
+
+
+$query = @'
+SELECT * 
+FROM tblHRIS_STAFF
+order by Staff_id desc
+'@
+
+
+[string]$ConnectionString = "Server=$SQLServer;Database=$Database;Integrated Security=True"
+$connection = New-Object -TypeName System.Data.SqlClient.SqlConnection
+$connection.ConnectionString = $connectionString
+$command = $connection.CreateCommand()
+			    
+$command.CommandText = $query
+$adapter = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter $command
+$dataset = New-Object -TypeName System.Data.DataSet
+$adapter.Fill($dataset) | Out-Null
+$Users = $dataset.Tables[0] 
+$connection.Close()   
+
+#Used to test if user exists already
+Function Get-User {
+    
+    param (
+				
+        [Parameter(Mandatory = $True,
+            Position = 1)]
+        [String]$Username
+    )
+
+    $DirectorySearcher = [adsisearcher]""
+    $DirectorySearcher.Filter = "samaccountname=$Username"
+    $Result = $DirectorySearcher.FindOne()
+    $Result.properties.samaccountname
+
+} 
+[INT]$UsersCount = ($Users | Measure-Object).count                         
+[INT]$CountUser = 0      
+foreach ($User in $Users) {
+    If ($ShowProgress) {
+        $CountUser++ 
+        Write-Progress -Activity "Adding new users" -Status "Checking $CountUser of $UsersCount, $($User.UserName)" -PercentComplete ($CountUser / $UsersCount * 100)
+    }
+    Write-Verbose "Checking $($user.Username)"    
+    If (Get-User -Username "$($user.Username)" ) {Write-Verbose "$($user.Username) found, skipping"}
+    Else {
+        Write-Verbose "$($user.Username) not found, creating"
+        $ComplexPassword = [System.Web.Security.Membership]::GeneratePassword(20, 10)
+        If ( $user.FirstName -and $user.Surname) {
+            $Initials = "$($user.FirstName)".Substring(0, 1)
+            $Initials += "$($user.Surname)".Substring(0, 1)
+            $Initials = $Initials.ToUpper()
+        }
+        Else {$Initials = ''}
+        New-Aduser -SamAccountName "$($user.Username)" -Name "$($user.FirstName) $($User.Surname)" -DisplayName "$($user.FirstName) $($User.Surname)" -GivenName "$($user.FirstName)" -Surname "$($user.Surname)" -Office "$($user.Location)" -EmployeeID "$($User.Staff_ID)" -Initials $Initials `
+            -UserPrincipalName "$($user.username+'@'+$Domain)" -AccountPassword (Convertto-securestring $ComplexPassword -asplainText -Force) -Enabled $true -Path $StagingOU -Department "$($User.Dept)" -Title "$($User.Position_Title)" -Company "$($User.Company)" -Credential $Credential -passthru | Select-Object -ExpandProperty  SamAccountName | out-file $LogPath -Append
+
+    }
+}
+                    
---
+++ Scripts/ActiveDirectory/Sync-HR_to_AD.ps1
@@ -0,0 +1,166 @@
+﻿<#
+     .SYNOPSIS
+          Syncs user properties from HR SQL into AD.
+     .DESCRIPTION
+          Syncs user attributes from HR SQL into AD. Can join on either SamAccountName or Employeeid.
+     .PARAMETER  Server
+          Remote DC to sync against.
+     .PARAMETER  Properties
+          User's properties to sync. Add new properties to validate set if needed. 
+     .PARAMETER	JoinOn
+	 	  Compare SamAccountName or EmployeeID.      
+	 .PARAMETER	SQLServer
+	 	  SQL server to import data from.
+     .PARAMETER	Database
+	 	  SQL server database containing data.
+     .PARAMETER	Credential
+	 	  Remote AD credentials.
+     .PARAMETER	ShowProgress
+	 	  Show progress if running from command line.          
+	 .EXAMPLE
+	      PS C:\> .\Sync-HR_to_AD.ps1 -properties Title, Department, Office, Enabled -Credential $creds
+		  Syncs users from HR into AD with default remote server.
+     .NOTES
+          Written by Aaron Ticehurst 7/4/2017. 
+#>
+
+[cmdletbinding(SupportsShouldProcess = $True)]
+param(
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 0)]
+    [String]$Server = 'ticehurst.local',
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 1)]
+    [ValidateSet("Title", "Department", "Office", "Enabled", "EmployeeID", "Givenname", "Surname", "Displayname", "Company")] 
+    [string[]]$Properties,
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 2)]
+    $RemoteOU = 'OU=ii,OU=People,OU=Staff,DC=ticehurst,DC=local',
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 3)]
+    [ValidateSet("SamAccountName", "EmployeeID")] 
+    [string]$JoinOn = 'SamAccountName',
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 4)]
+    [String]$SQLServer = 'sql2',
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 5)]
+    [String]$Database = 'ITS',
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 6)]
+    [pscredential]$Credential,
+    [parameter(Mandatory = $False,
+        ValueFromPipelineByPropertyName = $true,
+        ValueFromPipeline = $true,
+        Position = 7)]
+    [bool]$ShowProgress = $False
+)
+
+#requires -module ActiveDirectory
+
+if (-not (Test-Path c:\Logs\HR_Sync)) {New-Item -Path c:\Logs\HR_Sync -ItemType Directory}
+$LogFolder = "c:\Logs\HR_Sync"
+$LogFile = "HR_Sync.$(Get-date -format ddMMyyyyhhmmss).log"
+$LogPath = Join-Path -path $LogFolder -ChildPath $LogFile
+$NewLog = New-Item -Path $LogPath -ItemType File 
+Write-Verbose "LogFile: $LogPath on $env:ComputerName"
+
+[int]$JoinedUsers = 0
+
+#Change query to match what ever table is needed
+$Query = @'
+USE [ITS]
+SELECT *
+FROM tblHRIS_Staff
+'@	
+[string]$ConnectionString = "Server=$SQLServer;Database=$Database;Integrated Security=True"
+$connection = New-Object -TypeName System.Data.SqlClient.SqlConnection
+$connection.ConnectionString = $connectionString
+$command = $connection.CreateCommand()
+			    
+$command.CommandText = $query
+$adapter = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter $command
+$dataset = New-Object -TypeName System.Data.DataSet
+$adapter.Fill($dataset) | Out-Null
+$Users = $dataset.Tables[0] 
+$connection.Close()   
+
+#Need to Transform SQL columns to compare against Active Directory
+$LocalDCUsers = Write-output $Users | Select @{n = 'DisplayName'; e = {"$($_.firstName)" + " " + "$($_.Surname)"}}, @{n = 'SamAccountName'; e = {$_.Username}}, @{n = 'Enabled'; e = {if ($_.Active -eq 0) {'False'}
+        Else { 'True'}}
+}, `
+                @{n = 'EmployeeID' ; e = {$_.STAFF_ID}}, @{n = 'Title' ; e = {$_.POSITION_TITLE}}, @{n = 'Office' ; e = {$_.LOCATION}}, @{n = 'Department' ; e = {$_.Dept}}, @{n = 'GivenName'; e = {$_.FirstName}}, Surname, Company  
+#Clear the SQL query variable to free memory as no longer needed
+Clear-Variable Users
+Write-Verbose $LocalDCUsers.count              
+$RemoteDCUsers = @()
+
+$RemoteDCUsers = get-aduser -Filter {$JoinOn -like "*"} -SearchBase $RemoteOU -server $Server -Credential $Credential -Properties $Properties 
+Write-Verbose $RemoteDCUsers.count
+
+If ($LocalDCUsers -and $RemoteDCUsers) {
+
+    [INT]$RemoteUserCount = $RemoteDCUsers.Count
+    [INT]$CountUser = 0
+    $User_Property_Hash = New-Object System.Collections.Hashtable -ArgumentList $Properties.count
+
+    Foreach ($RemoteDCUser in $RemoteDCUsers) {
+        If ($ShowProgress) {
+            $CountUser++ 
+            Write-Progress -Activity "Syncing users" -Status "Checking $CountUser of $RemoteUserCount, $($RemoteDCUser.SamAccountName)" -PercentComplete ($CountUser / $RemoteUserCount * 100)
+        }
+        $MatchedHRUser = $null
+        If ($JoinOn -eq 'SamAccountName' ) {  $MatchedHRUser = $LocalDCUsers.Where( { $_.SamAccountName -eq $RemoteDCUser.SamAccountName })}
+        If ($JoinOn -eq 'EmployeeID' ) {  $MatchedHRUser = $LocalDCUsers.Where( { $_.EmployeeID -eq $RemoteDCUser.EmployeeID })}
+
+        If ($MatchedHRUser) {
+            
+            $JoinedUsers++
+                                   
+            Write-Verbose $RemoteDCUser
+            Write-Verbose "$($MatchedHRUser)"
+            Foreach ($Property in $Properties) { 
+                If ($MatchedHRUser.$Property -ne $RemoteDCUser.$Property -and $Property -ne 'Enabled') {
+                               
+                    Write-output "$($MatchedHRUser.SamAccountName) -> $($RemoteDCUser.Samaccountname) Set $Property from $($RemoteDcUser.$Property) to $($MatchedHRUser.$Property)" | out-file $LogPath -Append
+                    If ($MatchedHRUser.$Property -match '^$') {$User_Property_Hash.Add($Property, $Null)}  
+                    Else {$User_Property_Hash.Add($Property, $MatchedHRUser.$Property)}
+                }
+                #Active status needs to be handled separately due to type mismatch.
+                If ($MatchedHRUser.$Property -ne $RemoteDCUser.$Property -and $Property -eq 'Enabled') {
+                                   
+                    Write-output "$($MatchedHRUser.SamAccountName) -> $($RemoteDCUser.Samaccountname) Set $Property from $($RemoteDcUser.$Property) to $($MatchedHRUser.$Property)" | out-file $LogPath -Append
+                    [boolean]$status = if ($MatchedHRUser.$Property -eq 'false') {$False}
+                    Else {$True}
+                    $User_Property_Hash.Add($Property, $status)
+                } 
+
+                       
+            }
+            If ($User_Property_Hash.count -gt 0) { 
+                Write-Verbose $RemoteDCUser.Samaccountname 
+                Write-Verbose $User_Property_Hash                            
+                Set-ADUser -Identity $RemoteDCUser.Samaccountname -Credential $Credential -server $Server @User_Property_Hash 
+            }
+            $User_Property_Hash.Clear()
+        }
+    }
+}
+Write-Output "Number of users joined: $JoinedUsers" | out-file $LogPath -Append
+Write-Verbose "Log cleanup"
+
+Get-ChildItem -path $LogFolder | where {$_.LastWriteTime -le ((get-date).AddHours(-2))} | remove-item -recurse 
